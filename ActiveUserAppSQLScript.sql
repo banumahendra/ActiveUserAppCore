31b8fcdc78eacb1512e6237ef23622d71d66d493
Banu Mahendra <102462@ust-global.com>
2021-01-25 16:22:31 +0530

    DataBase Changes

---
+++ ActiveUserAppSQLScript.sql
@@ -0,0 +1,37 @@
+﻿/* Check for database exist */
+
+IF EXISTS 
+(
+     SELECT name FROM master.dbo.sysdatabases 
+    WHERE name = 'UserDB'
+)
+BEGIN
+    SELECT 'UserDB Database Name already Exist' AS Message
+END
+ELSE
+BEGIN
+    CREATE DATABASE [UserDB]
+    SELECT 'UserDB Database is Created'
+END
+
+GO
+
+USE [UserDB]
+
+GO
+/* Check for table exist */
+
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[User]') AND type in (N'U'))
+
+BEGIN
+CREATE TABLE [dbo].[User] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Name]     VARCHAR (100) NOT NULL,
+    [Email]    VARCHAR (100) NOT NULL,
+    [Phone]    VARCHAR (100) NOT NULL,
+    [IsActive] BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+END
+
